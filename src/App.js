02f0abe144ea4872cf79a54e5202e9368bd488e0
Katerina S <kslezakova12@gmail.com>
2022-06-24 10:18:35 +0200

    added html structure and classes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-primary">shopping list</h1>
-        <form>
-          <div className="row">
-            <div className="col-4">
-              <input type="text" className="form-control" />
+      <div className="App-wrapper">
+        <header className="App-header">
+          <h1 className="text-primary">shopping list</h1>
+        </header>
+        <section className="App-section">
+          <form>
+            <div className="row">
+              <div className="col-4">
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-2">
+                <button className="btn btn-outline-primary">search</button>
+              </div>
             </div>
-            <div className="col-2">
-              <button className="btn btn-outline-primary">search</button>
-            </div>
-          </div>
-        </form>
-      </header>
+          </form>
+        </section>
+        <footer className="App-footer">
+          This project is open-sourced on Github.
+        </footer>
+      </div>
     </div>
   );
 }
