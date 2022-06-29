816008495333f5ff283b2b4a8a41098c697caa11
Katerina S <kslezakova12@gmail.com>
2022-06-29 17:41:28 +0200

    styling of app

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-export default function List({ items,removeItem }) {
-
+export default function List({ items, removeItem }) {
   return (
     <div className="shopping-list">
       {items.map((item) => {
@@ -9,7 +8,12 @@ export default function List({ items,removeItem }) {
         return (
           <div className="shopping-item" key={id}>
             <h4>{name}</h4>
-            <button className="btn btn-outline-danger btn-sm" OnClick={() => removeItem(id)}>remove</button>
+            <button
+              className="btn btn-outline-danger btn-sm"
+              OnClick={() => removeItem(id)}
+            >
+              remove
+            </button>
           </div>
         );
       })}
