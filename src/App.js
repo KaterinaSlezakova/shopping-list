85f79069a9cbce431d250e6d8bb87115de9ff8d3
Katerina S <kslezakova12@gmail.com>
2022-06-29 17:16:20 +0200

    working on styling and functionality of app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import List from "./components/List";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+
+  const removeItem = (id) => {
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newItem = { id: new Date().getTime().toString(), name: item };
+    setList([...list, newItem]);
+    setItem("");
+  };
+
   return (
     <div className="App">
-      <div className="App-wrapper">
-        <header className="App-header">
-          <h1 className="text-primary">shopping list</h1>
-        </header>
-        <section className="App-section">
-          <form>
-            <div className="row">
-              <div className="col-4">
-                <input type="text" className="form-control" />
-              </div>
-              <div className="col-2">
-                <button className="btn btn-outline-primary">search</button>
-              </div>
+      <section className="App-section">
+        <h1 className="text-primary">Shopping list</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-10">
+              <input
+                type="text"
+                className="form-control"
+                value={item}
+                placeholder="Type an item.."
+                onChange={(e) => setItem(e.target.value)}
+              />
             </div>
-          </form>
-        </section>
-        <footer className="App-footer">
-          This project is open-sourced on Github.
-        </footer>
-      </div>
+            <div className="col">
+              <button type="submit" className="btn btn-primary">
+                Add
+              </button>
+            </div>
+          </div>
+        </form>
+        {list.length > 0 && (
+          <div className="item-container">
+            <List items={list} removeItem={removeItem} />
+            <button className="btn btn-danger text-center">Clear all</button>
+          </div>
+        )}
+      </section>
+      <footer className="App-footer text-center">
+        This project is open-sourced on Github.
+      </footer>
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function List({ items,removeItem }) {
+
+  return (
+    <div className="shopping-list">
+      {items.map((item) => {
+        const { id, name } = item;
+        return (
+          <div className="shopping-item" key={id}>
+            <h4>{name}</h4>
+            <button className="btn btn-outline-danger btn-sm" OnClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
