5a3e5ed39488a61c21e04c1b89c44f3a71dd8848
Katerina S <kslezakova12@gmail.com>
2022-07-01 09:30:39 +0200

    working on styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import "./App.css";
 import List from "./components/List";
 
@@ -19,36 +20,43 @@ function App() {
 
   return (
     <div className="App">
-      <section className="App-section">
-        <h1 className="text-primary">Shopping list</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-10">
-              <input
-                type="text"
-                className="form-control"
-                value={item}
-                placeholder="Type an item.."
-                onChange={(e) => setItem(e.target.value)}
-              />
+      <div className="container">
+        <div className="App-section shadow p-3 mb-5 bg-body rounded">
+          <h1 className="text-primary">Shopping list</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-10">
+                <input
+                  type="text"
+                  className="form-control"
+                  value={item}
+                  placeholder="Type an item.."
+                  onChange={(e) => setItem(e.target.value)}
+                />
+              </div>
+              <div className="col">
+                <button type="submit" className="btn btn-primary">
+                  Add
+                </button>
+              </div>
             </div>
-            <div className="col">
-              <button type="submit" className="btn btn-primary">
-                Add
+          </form>
+          {list.length > 0 && (
+            <div className="item-container">
+              <List items={list} removeItem={removeItem} />
+              <button
+                type="button"
+                className="btn btn-danger d-grid gap-2 col-6 mx-auto"
+              >
+                Clear all
               </button>
             </div>
-          </div>
-        </form>
-        {list.length > 0 && (
-          <div className="item-container">
-            <List items={list} removeItem={removeItem} />
-            <button className="btn btn-danger text-center">Clear all</button>
-          </div>
-        )}
-      </section>
-      <footer className="App-footer text-center">
-        This project is open-sourced on Github.
-      </footer>
+          )}
+        </div>
+        <footer className="App-footer text-center">
+          This project is open-sourced on Github.
+        </footer>
+      </div>
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -6,14 +6,23 @@ export default function List({ items, removeItem }) {
       {items.map((item) => {
         const { id, name } = item;
         return (
-          <div className="shopping-item" key={id}>
-            <h4>{name}</h4>
-            <button
-              className="btn btn-outline-danger btn-sm"
-              OnClick={() => removeItem(id)}
-            >
-              remove
-            </button>
+          <div className="shopping-container " key={id}>
+            <div className="shopping-item d-inline-flex justify-content-start">
+              <h4>{name}</h4>
+            </div>
+
+            <span className="btn-container d-inline-flex justify-content-end">
+              <button
+                type="button"
+                className="btn btn-outline-danger btn-sm"
+                OnClick={() => removeItem(id)}
+              >
+                Remove
+              </button>
+              <button type="button" class="btn btn-outline-warning btn-sm">
+                Edit
+              </button>
+            </span>
           </div>
         );
       })}
