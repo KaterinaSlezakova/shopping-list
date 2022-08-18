920c1efbf24bc595f74d32b4c2d3dbf7a2961aec
Katerina S <kslezakova12@gmail.com>
2022-08-18 08:22:19 +0200

    update styling of buttons

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ export default function App() {
             {Object.values(state.items).length > 0 && (
               <button
                 type="button"
-                className="btn btn-danger d-grid gap-2 col-8 mx-auto mt-3 shadow"
+                className="btn btn-danger btn-sm d-grid gap-2 col-8 mx-auto mt-3"
                 onClick={() =>
                   dispatch({
                     type: ACTIONS.CLEAR_ALL,
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaCheck, FaTrashAlt, FaEdit } from "react-icons/fa";
+import { FaCheck, FaTrash, FaEdit } from "react-icons/fa";
 import { ACTIONS } from "../actions";
 
 export default function Item({ item, dispatch }) {
@@ -7,7 +7,7 @@ export default function Item({ item, dispatch }) {
     <div className="Item">
       <div className="row pb-2 justify-content-center">
         <div
-          className="col-6"
+          className="col-4"
           style={{
             fontWeight: "bold",
             textDecoration: item.complete ? "line-through" : "none",
@@ -15,35 +15,21 @@ export default function Item({ item, dispatch }) {
         >
           <p>{item.name}</p>
         </div>
-        <div className="col-1">
+        <div className="col-2">
           <button
             type="button"
-            className="btn btn-secondary btn-sm shadow-lg"
+            className="btn btn-warning btn-sm"
             onClick={() =>
               dispatch({ type: ACTIONS.DELETE_ITEM, payload: { id: item.id } })
             }
           >
-            <FaTrashAlt />
+            <FaTrash />
           </button>
         </div>
-        <div className="col-1">
+        <div className="col-2">
           <button
             type="button"
-            className="btn btn-secondary btn-sm shadow-lg"
-            onClick={() =>
-              dispatch({
-                type: ACTIONS.TOGGLE_ITEM,
-                payload: { id: item.id },
-              })
-            }
-          >
-            <FaCheck />
-          </button>
-        </div>
-        <div className="col-1">
-          <button
-            type="button"
-            className="btn btn-secondary btn-sm shadow-lg"
+            className="btn btn-light btn-sm"
             onClick={() =>
               dispatch({
                 type: ACTIONS.EDIT_ITEM,
@@ -52,6 +38,20 @@ export default function Item({ item, dispatch }) {
             }
           >
             <FaEdit />
+          </button>
+        </div>
+        <div className="col-1">
+          <button
+            type="button"
+            className="btn btn-info btn-sm"
+            onClick={() =>
+              dispatch({
+                type: ACTIONS.TOGGLE_ITEM,
+                payload: { id: item.id },
+              })
+            }
+          >
+            <FaCheck />
           </button>
         </div>
       </div>
